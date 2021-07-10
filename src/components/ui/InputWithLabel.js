8c29f93e38a1518f25b4b0f93f302437f60db725
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-07-10 13:26:51 +0530

    feat: adding onChangeHandler to the input to bubble changes to parent

--- src/components/ui/InputWithLabel.js
+++ src/components/ui/InputWithLabel.js
@@ -1,9 +1,19 @@
-export default function InputWithLabel({ labelProps, inputProps }) {
-    const { label, ...remLabelProps } = labelProps
-    return (
-        <div className="flex flex-col">
-            <span className="text-sm text-gray-600" {...remLabelProps}>{label}</span>
-            <input className="border w-64 px-2 py-1 text-base" {...inputProps} />
-        </div>
-    )
+export default function InputWithLabel({
+  labelProps,
+  inputProps,
+  onChangeHandler,
+}) {
+  const { label, ...remLabelProps } = labelProps;
+  return (
+    <div className="flex flex-col">
+      <span className="text-sm text-gray-600" {...remLabelProps}>
+        {label}
+      </span>
+      <input
+        className="border w-64 px-2 py-1 text-base"
+        {...inputProps}
+        onChange={(e) => onChangeHandler(e.target.value)}
+      />
+    </div>
+  );
 }
