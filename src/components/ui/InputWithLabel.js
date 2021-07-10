52b486077fd5d6c348c9a133be62fd1e1cad209d
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-07-10 12:21:44 +0530

    style: minor style changes

--- src/components/ui/InputWithLabel.js
+++ src/components/ui/InputWithLabel.js
@@ -2,8 +2,8 @@ export default function InputWithLabel({ labelProps, inputProps }) {
     const { label, ...remLabelProps } = labelProps
     return (
         <div className="flex flex-col">
-            <span className="text-sm" {...remLabelProps}>{label}</span>
-            <input className="border w-64" {...inputProps} />
+            <span className="text-sm text-gray-600" {...remLabelProps}>{label}</span>
+            <input className="border w-64 px-2 py-1 text-base" {...inputProps} />
         </div>
     )
 }
