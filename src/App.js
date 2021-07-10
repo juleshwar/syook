9cd50195b61cacea3bbe0799a43171d85dc1e946
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-07-10 17:49:24 +0530

    refactor: removing irrelevant code for now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import { useSelector } from "react-redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import DashboardPage from "./components/DashboardPage";
 import LoginPage from "./components/LoginPage";
 
 function App() {
-  // const isLoggedIn = useSelector((state) => state.currentUser.loggedIn);
-  // console.log(isLoggedIn);
   return (
     <BrowserRouter>
       <Switch>
