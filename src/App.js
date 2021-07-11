fa25314406bff96e6179699f54919718211d9867
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-07-11 16:41:16 +0530

    refactor: moving BrowserRouter wrapper to parent

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route, withRouter } from "react-router-dom";
 import DashboardPage from "./components/DashboardPage";
 import LoginPage from "./components/LoginPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={LoginPage}></Route>
-        <Route exact path="/login" component={LoginPage}></Route>
-        <Route exact path="/dashboard" component={DashboardPage}></Route>
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route exact path="/login" component={LoginPage}></Route>
+      <Route exact path="/dashboard" component={DashboardPage}></Route>
+    </Switch>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import store from "./store";
 import "./styles.css";
@@ -9,7 +10,9 @@ var mountNode = document.getElementById("app");
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   mountNode
