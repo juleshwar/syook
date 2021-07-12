2563c812e7c25daf6093d9dadc74996eff598d3b
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-07-12 14:42:04 +0530

    feat: adding a VoteBlock to each dish
    @func getVoteBlockState computes the visual state of each block. It shows whether a certain vote is selected, already cast or uncasted

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { withRouter } from "react-router-dom";
+import { VOTE } from "../constants/vote";
 import { usersActions } from "../store/slices/users";
+import { dishesActions } from "../store/slices/dishes";
+import VoteBlock, { VOTE_STATE } from "./ui/VoteBlock";
 
 function DashboardPage({ history }) {
   const dishes = useSelector((state) => state.dishes);
@@ -55,6 +58,27 @@ function DashboardPage({ history }) {
     dispatch(dishesActions.setRank({ dishId, rank }));
   }
 
+  function getVoteBlockState(votedDishes, dishId) {
+    if (!isLoggedIn) {
+      return {};
+    }
+    return Object.entries(VOTE).reduce((acc, [key, v]) => {
+      /**
+       * verdict returns SELECTED if the currentUser has voted @param v for @param dishId
+       * verdict returns DIMMED if the currentUser has already voted @param v for another dish
+       * verdict returns UNSELECTED if the currentUser has NOT voted @param v for any dish
+       */
+      const verdict =
+        votedDishes[v] === dishId
+          ? VOTE_STATE.SELECTED
+          : votedDishes[v]
+          ? VOTE_STATE.DIMMED
+          : VOTE_STATE.UNSELECTED;
+      acc[v] = verdict;
+      return acc;
+    }, {});
+  }
+
   return (
     <section className="flex flex-col items-center">
       <nav className="flex w-full h-16 text-lg items-center justify-between bg-red-100 p-4">
@@ -71,6 +95,7 @@ function DashboardPage({ history }) {
               <td className={headerCellClasses}>Image</td>
               <td className={headerCellClasses}>Dish</td>
               <td className={headerCellClasses}>Points</td>
+              <td className={headerCellClasses}>Vote Here</td>
             </tr>
           </thead>
           <tbody>
@@ -81,6 +106,14 @@ function DashboardPage({ history }) {
                 </td>
                 <td className={rowCellClasses}>{dish.dishName}</td>
                 <td className={rowCellClasses}>{dish.points}</td>
+                <td className={rowCellClasses}>
+                  <VoteBlock
+                    state={getVoteBlockState(votedDishes, dish.id)}
+                    onClickHandler={(rank) =>
+                      onVote(votedDishes, dish.id, rank)
+                    }
+                  />
+                </td>
               </tr>
             ))}
           </tbody>
