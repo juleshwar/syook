66b91c82a91ac56b50de189735607916e89058a6
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-07-12 17:06:44 +0530

    feat: adding 'classes' prop for Button

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-export default function Button({ buttonProps = {}, children }) {
+export default function Button({ buttonProps = {}, children, classes }) {
   const { primary, secondary, ...remButtonProps } = buttonProps;
   return (
     <button
       className={`border 
         ${primary ? "bg-green-400" : ""} 
         ${secondary ? "bg-blue-50" : ""}
-        bg-gray-100 p-2 rounded
+        p-2 rounded 
+        ${classes}
         `}
       {...remButtonProps}
     >
